b9a26bde94e371dca9477a734415d4aac396462e
matt <matt@mattunderscore.co>
2016-05-21 22:50:40 -0700

    update with current site stuff

--- js/streamStatus.js
+++ js/streamStatus.js
@@ -1,10 +1,17 @@
-$(document).ready(function() {    
-   $.getJSON("https://api.twitch.tv/kraken/streams/PandaPlaysHD?callback=?",function(streamData) {
+$(document).ready(function() {   
+
+	var twitchName = "pandaplayshd";
+	var youtubeName = "pandaplaysmchd";
+   $.getJSON("https://api.twitch.tv/kraken/streams/" + twitchName + "?callback=?",function(streamData) {
       console.log('Stream Data:', streamData)
       if(streamData && streamData.stream) {
-         $('#streamWidget').html("<b>[LIVE]</b>")
+         $.getJSON('https://api.twitch.tv/kraken/channels/' + twitchName + '?callback=?', function(data) {
+			document.getElementById('title').textContent = "Stream is live - \"" + data.status + "\"";
+			document.getElementById('player').src = "https://player.twitch.tv/?channel=" + twitchName;
+});
       } else {
-         $('#streamWidget').html("")
+         document.getElementById('title').textContent = "Stream is offline - Most Recent YouTube Video:";
+		 document.getElementById('player').src = "https://www.youtube.com/embed?listType=user_uploads&list=" + youtubeName;
       }
    })
 })
